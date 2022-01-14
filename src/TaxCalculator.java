b62c7ed9f9f0d823e5226a1d988df4ce7a8f3c93
Prachip9767 <prachip293@gmail.com>
2022-01-14 19:44:29 +0530

    Tax calculator

--- src/TaxCalculator.java
+++ src/TaxCalculator.java
@@ -18,8 +18,8 @@ public class TaxCalculator {
         else if (salary <= 60000)
             taxAmount = 0.1 * (salary - 50000); //  10% Tax= 10/100 =0.1.
         else if (salary <= 150000)
-            taxAmount = (0.2 * (salary - 60000) + 1000); // 10% on additional amount
-        else taxAmount = (0.3 * (salary - 150000) +19000); //10% + 20% on additional amount
+            taxAmount = 0.2 * (salary - 60000) + 1000; // 10% on additional amount
+        else taxAmount = 0.3 * (salary - 150000) +19000; //10% + 20% on additional amount
 
         System.out.println("Your Total income tax is:-"+taxAmount);
     }
