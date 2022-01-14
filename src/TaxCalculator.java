0bc34abfa028abf819e2955f0df5a31321c9231e
Prachip9767 <prachip293@gmail.com>
2022-01-14 19:42:36 +0530

    Tax calculator

--- src/TaxCalculator.java
+++ src/TaxCalculator.java
@@ -1,29 +1,26 @@
 /*
     Write a java program which initialize the earning of an employee. The program should calculate the income
     tax to be paid by the employee as per the criteria given below
+
 */
 import java.util.Scanner;
 public class TaxCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("**** Welcome to Tax Calculator ****");
+
         System.out.println("Enter You Salary below");
         double salary = scanner.nextInt();
-        System.out.println("Your Total income tax is:-"+CalculateTax(salary));
-    }
 
-    static double CalculateTax(double salary) {
         double taxAmount ;
-        if (salary <= 50000)
+        if (salary <= 50000) // Nil
             taxAmount = 0;
         else if (salary <= 60000)
-            taxAmount = 0.1 * (salary - 50000);
+            taxAmount = 0.1 * (salary - 50000); //  10% Tax= 10/100 =0.1.
         else if (salary <= 150000)
             taxAmount = (0.2 * (salary - 60000) + 1000); // 10% on additional amount
-        else taxAmount = (0.3 * (salary - 150000) + 1000 + 18000); //10% + 20% on additional amount
-        return taxAmount;
+        else taxAmount = (0.3 * (salary - 150000) +19000); //10% + 20% on additional amount
+
+        System.out.println("Your Total income tax is:-"+taxAmount);
     }
 }
-/*
-    10/100 =0.1.
- */
